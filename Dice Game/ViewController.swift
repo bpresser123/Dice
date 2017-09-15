ec4a9f290b0897eb5d12d520b53a4d88a937a6a5
Brian Presser <bp@bp-dl.local>
2017-09-14 23:50:19 -0400

    Adding dice images and roll button

---
+++ Dice Game/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dice Game
+//
+//  Created by Brian Presser on 9/14/17.
+//  Copyright © 2017 Brian Presser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+
+}
+
